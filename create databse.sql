4620b7312047392f8875f19d7bdce92455bf9c07
roisingoodman1 <43958072+roisingoodman1@users.noreply.github.com>
2018-10-08 14:08:03 +0100

    Update create databse.sql

--- create databse.sql
+++ create databse.sql
@@ -6,14 +6,14 @@ employeeName VARCHAR(30) not null,
 employeeAddress VARCHAR(100),
 NINumber VARCHAR(9) not null,
 employeeBank VARCHAR(36) not null,
-employeeSalary DECIMAL(11))
-employeeDept VARCHAR(20);
+employeeSalary DECIMAL(11),
+employeeDept VARCHAR(20));
 
 delimiter //
 create procedure testData()
 
 BEGIN
-	INSERT into EmployeeDetails VALUES (, "James Matchett", "Belfast", "PE284243G", "1234567890", 100000.00, 'Evolve');
+	INSERT into EmployeeDetails VALUES (1, "James Matchett", "Belfast", "PE284243G", "1234567890", 100000.00, 'Evolve');
 	INSERT into EmployeeDetails VALUES (2, "Conor McCormick", "Larne", "PE234823G", "12513787890", 200000.00, 'Systems');
 	INSERT into EmployeeDetails VALUES (3, "Dylan Robinson", "Strabane", "PE245J43G", "123258367890", 300000.00, 'Workday');
 	INSERT into EmployeeDetails VALUES (4, "Maeve Donnelly", "Portadown", "PE2HJD84G", "1568367890", 400000.00, 'People');
